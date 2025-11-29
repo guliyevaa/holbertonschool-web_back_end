0264ce6060555984e316c6a88e1c6e61af176f8e
guliyevaa <12545@holbertonstudents.com>
2025-11-29 13:28:21 +0000

    .

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+// my comment
+def list_all(mongo_collection):
+    """
+    List all documents in a MongoDB collection
+    """
+    docs = list(mongo_collection.find())
+    return docs
